e9bfe6d8c15066f63646a90db71df25cff5ee9da
srinivasankalap <cnu.ankalap@gmail.com>
2023-10-12 11:32:04 +0530

    Nested routes

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "edfd583e820b392b"; }
+/******/ 		__webpack_require__.h = function() { return "ccb76d7709b6bc02"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
